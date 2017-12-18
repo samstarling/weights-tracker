7b97bfcb3cbfa7de6eb981ae2c41d0630b589ecd
Sam Starling <mail@samstarling.co.uk>
2017-12-18 17:17:49 +0100

    Allow basic editing of a list of exercises and weights

--- src/index.js
+++ src/index.js
@@ -2,58 +2,106 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Exercise extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'foo',
-      weight: 50.0,
-      reps: 8,
-      sets: 3
-    };
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
   }
-
-  render() {
-    return (
-      <div class="cl">
-        <div class="fl w-50"><input value={this.state.name}/></div>
-        <div class="fl w-20">{this.state.weight}kg</div>
-        <div class="fl w-30">{this.state.reps} &times; {this.state.sets}</div>
-      </div>
-    )
-  }
-}
-
-class Grouping extends React.Component {
-  render() {
-    return (
-      <div class="bb pb4 mb4">
-        <h2 class="f3 mb2">{this.props.name}</h2>
-        <Exercise/>
-        <Exercise/>
-        <Exercise/>
-        <button class="mt2" type="button">Add exercise</button>
-      </div>
-    )
-  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
 
 class List extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items
+    }
+  }
+
+  updateName(id, name) {
+    this.setState({
+      items: this.state.items.map( (item) => {
+        if(id === item.id) {
+          return {
+            ...item,
+            name
+          }
+        }
+        return item;
+      })
+    });
+  }
+
+  updateWeight(id, weight) {
+    this.setState({
+      items: this.state.items.map( (item) => {
+        if(id === item.id) {
+          return {
+            ...item,
+            weight
+          }
+        }
+        return item;
+      })
+    });
+  }
+
+  addItem(text) {
+    let newItem = { id: guid(), name: 'Untitled', weight: 0 }
+    this.setState({ items: this.state.items.concat(newItem)})
+  }
+
   render() {
     return (
       <div>
         <h1 class="f1">Exercises</h1>
-        <Grouping name="Upper body"/>
-        <Grouping name="Lower body"/>
-        <button type="button">Add grouping</button>
+        <ul>
+        {
+          this.state.items.map((item) =>
+            <Item id={item.id}
+              name={item.name}
+              weight={item.weight}
+              updateName={this.updateName.bind(this)}
+              updateWeight={this.updateWeight.bind(this)} />) }
+        </ul>
+        <a href="#" onClick={() => this.addItem('Testing')}>Add</a>
+        <DebugArea data={JSON.stringify(this.state.items, null, 2)}/>
       </div>
     )
   }
 }
 
-// ========================================
+class Item extends React.Component {
+  render() {
+    return (
+      <li>
+        <input class="w-60" type="text"
+          value={ this.props.name }
+          onChange={(event) => this.props.updateName(this.props.id, event.target.value)}/>
+        <input class="w-20" type="text"
+          value={ this.props.weight }
+          onChange={(event) => this.props.updateWeight(this.props.id, event.target.value)}/>
+      </li>
+    )
+  }
+}
+
+class DebugArea extends React.Component {
+  render() {
+    return (
+      <div class="bg-near-white w-100 f6 mt4 pa2"><pre class="ma0 overflow-x-scroll">{ this.props.data }</pre></div>
+    )
+  }
+}
+
+const ITEMS = [
+  { id: guid(), name: "Bench Press", weight: 42.5 },
+  { id: guid(), name: "Chest Press", weight: 30 }
+]
 
 ReactDOM.render(
-  <List />,
+  <List items={ITEMS} />,
   document.getElementById('root')
 );
